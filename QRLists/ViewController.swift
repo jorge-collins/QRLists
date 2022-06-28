a3e06980334b58ed5265ceb78df0e95257cf1789
jorge-collins <jorge.collins@gmail.com>
2022-06-28 10:28:32 -0600

    Initial Commit

---
+++ QRLists/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QRLists
+//
+//  Created by Jorge Collins Gómez on 28/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
